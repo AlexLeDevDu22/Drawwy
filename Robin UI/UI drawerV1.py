109f7cee8d30370276b722d92aa8c265ecec9827
robin.loisil22 <robin.loisil22@gmail.com>
2025-02-14 18:05:33 +0100

    UIDRAWER

--- Robin UI/UI drawerV1.py
+++ Robin UI/UI drawerV1.py
@@ -1,8 +1,9 @@
 import pygame
 pygame.init()
 def UIdrawer(): 
+    info_ecran = pygame.display.Info()
+    largeur, hauteur = info_ecran.current_w, info_ecran.current_h
     # Dimensions de la fenêtre
-    largeur, hauteur = 0, 0
     ecran = pygame.display.set_mode((largeur, hauteur))
     pygame.display.set_caption("UIdrawer")
 
@@ -12,12 +13,12 @@ def UIdrawer():
 
 
     zones = [
-        (17.7, 4.17, 44.27, 72.5),  # Zone de dessin
-        (1, 4.17, 17.18, 58.33),   # Liste personnes
-        (1,64.17, 17.18, 12.5),   # Mot à deviner
-        (63, 4.17, 17.18, 25),  # Couleurs
-        (63, 30.83, 17.18, 8.33),  # Style de stylo
-        (63, 40.83, 17.18, 35.83), # Chat
+        (20/100*largeur, 4.17/100*hauteur, 60/100*largeur, 91/100*hauteur),  # Zone de dessin
+        (1/100*largeur, 4.17/100*hauteur, 18/100*largeur, 70/100*hauteur),   # Liste personnes
+        (1/100*largeur,75/100*hauteur, 18/100*largeur, 20/100*hauteur),   # Mot à deviner
+        (81/100*largeur, 4.17/100*hauteur, 18/100*largeur, 25/100*hauteur),  # Couleurs
+        (81/100*largeur, 30.83/100*hauteur, 18/100*largeur, 8.33/100*hauteur),  # Style de stylo
+        (81/100*largeur, 40.83/100*hauteur, 18/100*largeur, 54.5/100*hauteur), # Chat
     ]
 
 
