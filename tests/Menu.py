72d3fae6758d9d648aa2bdc7c8684c0309b7743e
robin.loisil22 <robin.loisil22@gmail.com>
2025-03-07 15:29:06 +0100

    menu

--- tests/Menu.py
+++ tests/Menu.py
@@ -19,8 +19,8 @@ black = (0, 0, 0)
 gray = (200, 200, 200)
 
 
-font = pygame.font.SysFont(None, 55)
-small_font = pygame.font.SysFont(None, 35)
+font = pygame.font.SysFont(None, 300)
+small_font = pygame.font.SysFont(None, 80)
 
 # Fonction pour dessiner le texte
 def draw_text(text, font, color, surface, x, y):
@@ -40,18 +40,18 @@ while running:
     ecran.fill(white)
 
     # Dessiner le titre
-    draw_text("DRAWWY", font, black, ecran, (largeur, hauteur)[0] // 2, 50)
+    draw_text("DRAWWY", font, black, ecran, (largeur, hauteur)[0]//2 , 200)
 
     # Dessiner les boutons
     buttons = ["JOUER", "SUCCES", "PARAMETRES", "QUITTER"]
-    button_y_start = 120
-    button_height = 50
-    button_width = 200
+    button_y_start = 400
+    button_height = 100
+    button_width = 500
     button_x = ((largeur, hauteur)[0] - button_width) // 2
 
     for i, button_text in enumerate(buttons):
-        button_y = button_y_start + i * button_height
-        pygame.draw.rect(ecran, gray, (button_x, button_y, button_width, button_height), border_radius=10)
+        button_y = button_y_start + i*1.4 * button_height
+        pygame.draw.rect(ecran, gray, (button_x, button_y, button_width, button_height), border_radius=100)
         draw_text(button_text, small_font, black, ecran, button_x + button_width // 2, button_y + button_height // 2)
 
     # Mettre à jour l'affichage
